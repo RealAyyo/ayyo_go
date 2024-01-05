ce99f99c679ed025dfec3d2492cba1a0412251c7
ayyo <fenixwhy@gmail.com>
2024-01-05 18:15:16 +0300

    решение задания

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,64 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: go-telnet [--timeout=10s] host port")
+		os.Exit(1)
+	}
+
+	host := args[0]
+	port := args[1]
+	address := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting: %v", err)
+		os.Exit(1)
+	}
+
+	defer client.Close()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	defer stop()
+
+	go func() {
+		err := client.Receive()
+		if err == nil {
+			fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+			stop()
+		}
+	}()
+
+	go func() {
+		err := client.Send()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("...EOF")
+			}
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,62 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *Client) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
+}
+
+func (c *Client) Send() error {
+	scanner := bufio.NewScanner(c.in)
+	for scanner.Scan() {
+		text := fmt.Sprintf("%s\n", scanner.Text())
+
+		_, err := c.conn.Write([]byte(text))
+		if err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
+
+func (c *Client) Receive() error {
+	scanner := bufio.NewScanner(c.conn)
+	for scanner.Scan() {
+		text := fmt.Sprintf("%s\n", scanner.Text())
+		_, err := c.out.Write([]byte(text))
+		if err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
