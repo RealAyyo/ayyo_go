da92debd3fb650525f75738a4001e072a1789268
ayyo <fenixwhy@gmail.com>
2024-01-08 00:15:45 +0300

    2) Логирование в сервисе

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,66 @@
 package logger
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+)
 
-type Logger struct { // TODO
+type Logger struct {
+	logger *slog.Logger
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+func New(level string) Logger {
+	var slogLevel slog.Level
+	switch level {
+	case "INFO":
+		slogLevel = slog.LevelInfo
+		break
+	case "WARN":
+		slogLevel = slog.LevelWarn
+		break
+	case "DEBUG":
+		slogLevel = slog.LevelDebug
+		break
+	case "ERROR":
+		slogLevel = slog.LevelError
+		break
+	}
+	var log *slog.Logger
+
+	log = slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slogLevel}))
+
+	return Logger{logger: log}
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+func (l Logger) Info(msg string, attrs ...any) {
+	if len(attrs) > 0 {
+		l.logger.Info(msg, attrs)
+	} else {
+		l.logger.Info(msg)
+	}
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+func (l Logger) Error(msg string, attrs ...any) {
+	if len(attrs) > 0 {
+		l.logger.Error(msg, attrs)
+	} else {
+		l.logger.Error(msg)
+	}
 }
 
-// TODO
+func (l Logger) Debug(msg string, attrs ...any) {
+	if len(attrs) > 0 {
+		l.logger.Debug(msg, attrs...)
+	} else {
+		l.logger.Debug(msg)
+	}
+}
+
+func (l Logger) Warn(msg string, attrs ...any) {
+	if len(attrs) > 0 {
+		l.logger.Warn(msg, attrs)
+	} else {
+		l.logger.Warn(msg)
+	}
+}
